b75ccf7061d25ae1aadfee73db7dd92db53a5613
Harish.K <kollatiharish@gmail.com>
2025-02-07 21:15:41 +0530

    implemented the sign/signup form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/Netflix_Logo_PMS.png"
 const Header = () => {
   return (
     <>
-      <div className='absolute px-8 py-2 bg-gradient-to-b from-black'>
+      <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10'>
         <img src={logo} alt="logo" className='w-40'/>
       </div>
     </>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
-import netflixBg from '../assets/netflix_bg.jpg'
-import Header from './Header'
+import React, { useState } from "react";
+import netflixBg from "../assets/netflix_bg.jpg";
+import Header from "./Header";
 
 const Login = () => {
+  const [isSignInform,setIsSignInform] = useState(false);
+
+  const toggleSignIn = () =>{
+    setIsSignInform(!isSignInform);
+  }
+
   return (
     <div>
-      <Header/>
-      <div className=''>
-        <img src={netflixBg} alt="Netflix_background_image"/>
+      <Header />
+      <div className="absolute">
+        <img src={netflixBg} alt="Netflix_background_image" />
       </div>
+      <form className="max-w-[25rem] absolute py-16 px-12 my-32 bg-black text-white left-0 right-0 mx-auto bg-opacity-80 rounded">
+        <h1 className="mb-14 text-3xl font-semibold">{!isSignInform ? "Sign In" : "Sign Up"}</h1>
+        {!isSignInform && <input
+          type="text"
+          placeholder="Name"
+          className="w-full mb-7 px-4 py-2 rounded bg-gray-600"
+        />}
+        <input
+          type="text"
+          placeholder="Email or Mobile Number"
+          className="w-full mb-7 px-4 py-2 rounded bg-gray-600"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full mb-7 px-4 py-2 rounded bg-gray-600"
+        />
+        <button type="submit" className="w-full bg-red-700 py-2 mb-7 rounded">
+          Sign In
+        </button>
+        {!isSignInform ? <p>
+          New to Netflix? <span className="font-bold cursor-pointer" onClick={toggleSignIn}>Sign up now</span>.
+        </p> : <p>Already have account? <span className="font-bold cursor-pointer" onClick={toggleSignIn}>Sign In now</span></p>}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
